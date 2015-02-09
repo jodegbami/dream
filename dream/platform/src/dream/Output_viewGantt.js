8f3556595776390a89c13f57b769460392d03d0c
Jérome Perrin <jerome@nexedi.com>
2015-02-09 13:44:07 +0100

    WIP on Gantt

--- dream/platform/src/dream/Output_viewGantt.js
+++ dream/platform/src/dream/Output_viewGantt.js
@@ -207,38 +207,139 @@
         gadget = this;
       gadget.props.jio_key = jio_key;
       gadget.props.result = options.result;
-      gadget.props.action = options.action;
+      gadget.props.action_definition = options.action_definition;
 
       return gadget.aq_getAttachment({
-        "_id": gadget.props.jio_key,
-        "_attachment": "body.json"
-      })
+          "_id": gadget.props.jio_key,
+          "_attachment": "body.json"
+        })
         .push(function (simulation_json) {
           var json_data = JSON.parse(simulation_json);
-          gadget.props.result = gantt_widget(
-            json_data,
-            gadget
-          );
+          gadget.props.result = json_data.result.result_list[options.result].sample_gantt;
         });
     })
-
     .declareMethod("startService", function () {
+      console.log('start', this.props.result);
+      
+      
+      console.log(gantt);
+      
+var demo_tasks = {
+	data:[
+		{"id":1, "text":"Office itinerancy", "type":gantt.config.types.project, "order":"10", progress: 0.4, open: false},
 
-      $(this.props.element).find('.gant_container').dhx_gantt({
-        data: this.props.result,
-        readonly: true,
-        /* for days has simulation time unit
-        scale_unit: 'day',
-        step: 7
-        */
-          // for hours has simulation time unit
-        scale_unit: 'day',
-        duration_unit: 60 * 1000,
-          // date_grid: "%H:%i",
-        date_scale: "%M/%d",
-        step: 1,
-        subscales: [{unit: "hour", step: 1, date: "%H:%i"}]
-      });
+		{"id":2, "text":"Office facing", "type":gantt.config.types.project, "start_date":"02-04-2013", "duration":"8", "order":"10", progress: 0.6, "parent":"1", open: true},
+		{"id":3, "text":"Furniture installation", "type":gantt.config.types.project, "start_date":"11-04-2013", "duration":"8", "order":"20", "parent":"1", progress: 0.6, open: true},
+		{"id":4, "text":"The employee relocation", "type":gantt.config.types.project, "start_date":"13-04-2013", "duration":"6", "order":"30", "parent":"1", progress: 0.5, open: true},
+
+        {"id":5, "text":"Interior office", "start_date":"02-04-2013", "duration":"7", "order":"3", "parent":"2", progress: 0.6, open: true},
+        {"id":6, "text":"Air conditioners check", "start_date":"03-04-2013", "duration":"7", "order":"3", "parent":"2", progress: 0.6, open: true},
+        {"id":7, "text":"Workplaces preparation", "start_date":"11-04-2013", "duration":"8", "order":"3", "parent":"3", progress: 0.6, open: true},
+        {"id":8, "text":"Preparing workplaces", "start_date":"14-04-2013", "duration":"5", "order":"3", "parent":"4", progress: 0.5, open: true},
+        {"id":9, "text":"Workplaces importation", "start_date":"14-04-2013", "duration":"4", "order":"3", "parent":"4", progress: 0.5, open: true},
+        {"id":10, "text":"Workplaces exportation", "start_date":"14-04-2013", "duration":"3", "order":"3", "parent":"4", progress: 0.5, open: true},
+
+        {"id":11, "text":"Product launch", "type":gantt.config.types.project, "order":"5", progress: 0.6, open: true},
+
+        {"id":12, "text":"Perform Initial testing", "start_date":"03-04-2013", "duration":"5", "order":"3", "parent":"11", progress: 1, open: true},
+        {"id":13, "text":"Development", "type":gantt.config.types.project, "start_date":"02-04-2013", "duration":"7", "order":"3", "parent":"11", progress: 0.5, open: true},
+        {"id":14, "text":"Analysis", "start_date":"02-04-2013", "duration":"6", "order":"3", "parent":"11", progress: 0.8, open: true},
+        {"id":15, "text":"Design", "type":gantt.config.types.project, "start_date":"02-04-2013", "duration":"5", "order":"3", "parent":"11", progress: 0.2, open: false},
+        {"id":16, "text":"Documentation creation", "start_date":"02-04-2013", "duration":"7", "order":"3", "parent":"11", progress: 0, open: true},
+
+        {"id":17, "text":"Develop System", "start_date":"03-04-2013", "duration":"2", "order":"3", "parent":"13", progress: 1, open: true},
+
+		{"id":25, "text":"Beta Release", "start_date":"06-04-2013", "order":"3","type":gantt.config.types.milestone, "parent":"13", progress: 0, open: true},
+
+        {"id":18, "text":"Integrate System", "start_date":"08-04-2013", "duration":"2", "order":"3", "parent":"13", progress: 0.8, open: true},
+        {"id":19, "text":"Test", "start_date":"10-04-2013", "duration":"4", "order":"3", "parent":"13", progress: 0.2, open: true},
+        {"id":20, "text":"Marketing", "start_date":"10-04-2013", "duration":"4", "order":"3", "parent":"13", progress: 0, open: true},
+
+        {"id":21, "text":"Design database", "start_date":"03-04-2013", "duration":"4", "order":"3", "parent":"15", progress: 0.5, open: true},
+        {"id":22, "text":"Software design", "start_date":"03-04-2013", "duration":"4", "order":"3", "parent":"15", progress: 0.1, open: true},
+        {"id":23, "text":"Interface setup", "start_date":"03-04-2013", "duration":"5", "order":"3", "parent":"15", progress: 0, open: true},
+        {"id":24, "text":"Release v1.0", "start_date":"15-04-2013", "order":"3","type":gantt.config.types.milestone, "parent":"11", progress: 0, open: true}
+	],
+	links:[
+		{id:"1",source:"1",target:"2",type:"1"},
+
+		{id:"2",source:"2",target:"3",type:"0"},
+		{id:"3",source:"3",target:"4",type:"0"},
+		{id:"4",source:"2",target:"5",type:"2"},
+		{id:"22",source:"13",target:"24",type:"0"}
+	]
+};
+
+		gantt.templates.scale_cell_class = function(date){
+		    if(date.getDay()==0||date.getDay()==6){
+		        return "weekend";
+		    }
+		};
+		gantt.templates.task_cell_class = function(item,date){
+		    if(date.getDay()==0||date.getDay()==6){ 
+		        return "weekend" ;
+		    }
+		};
+
+		gantt.templates.rightside_text = function(start, end, task){
+			if(task.type == gantt.config.types.milestone){
+				return task.text;
+			}
+			return "";
+		}
+
+		gantt.config.columns = [
+		    {name:"text",       label:"Task name",  width:"*", tree:true },
+		    {name:"start_time",   label:"Start time",  template:function(obj){
+				return gantt.templates.date_grid(obj.start_date);
+		    }, align: "center", width:60 },
+		    {name:"duration",   label:"Duration", align:"center", width:60}
+		    
+		];
+
+/*
+		gantt.config.grid_width = 390;
+		gantt.config.date_grid = "%F %d"
+		gantt.config.scale_height  = 60;
+		gantt.config.subscales = [
+			{ unit:"week", step:1, date:"Week #%W"}
+		];
+		
+		http://docs.dhtmlx.com/gantt/desktop__configuring_time_scale.html#settingtheunitofthescale
+		
+		
+*/
+//		gantt.config.start_date = new Date(2013, 10, 4, 7, 00);
+//		gantt.config.end_date = new Date(2013, 10, 4, 24, 00);
+		gantt.config.date_grid = "%H:%i";
+		gantt.config.scale_unit = "hour";
+		gantt.config.step = 4;
+		gantt.config.subscales = [
+			{ unit:"day", step:1, date:"%F %d"}
+		];
+		//gantt.config.duration_unit = "hour";
+		gantt.config.date_scale = "%H:%i";
+
+
+    gantt.init($(this.props.element).find('.gant_container')[0]);
+		//modSampleHeight();
+		gantt.parse(demo_tasks);
+
+//      $(this.props.element).find('.gant_container').dhx_gantt({
+//        data: this.props.result,
+//        readonly: true,
+//        /* for days has simulation time unit
+//        scale_unit: 'day',
+//        step: 7
+//        */
+//          // for hours has simulation time unit
+//        scale_unit: 'day',
+//        duration_unit: 60 * 1000,
+//          // date_grid: "%H:%i",
+//        date_scale: "%M/%d",
+//        step: 1,
+//        subscales: [{unit: "hour", step: 1, date: "%H:%i"}]
+//      });
 
       return new RSVP.Queue()
         .push(function () {
